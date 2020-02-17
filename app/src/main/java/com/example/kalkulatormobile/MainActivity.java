fed931034151f4c7346f5723d17d91b6c55bab9d
kresnaiqbal <mohammad.iqbal.krisna.tif18@polban.ac.id>
2020-02-17 15:06:40 +0700

    first commit

---
+++ app/src/main/java/com/example/kalkulatormobile/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kalkulatormobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText TextNama;
+    TextView Hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Panggil variable berdasarkan id
+        TextNama=(EditText)findViewById(R.id.TxtNama);
+        Hasil=(TextView)findViewById(R.id.Lbl_Nama);
+    }
+
+    public void TampilNama (View v){
+        Hasil.setText ("Nama Anda: "+TextNama.getText());
+    }
+}
